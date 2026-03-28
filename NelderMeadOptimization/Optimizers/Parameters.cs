f4fc51c4469922845936f04d70f3ef764e52a976
Merem05 <mashaeremenko2005@gmail.com>
2026-03-28 15:26:54 +0300

    Update Parameters.cs
    Пока что сделала так, record  не поддерживается моей версией  vs

--- NelderMeadOptimization/Optimizers/Parameters.cs
+++ NelderMeadOptimization/Optimizers/Parameters.cs
@@ -1,32 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace NelderMeadOptimization.Optimizers
 {
     internal class Parameters
     {
-        public double Alpha { get; set; } = 1.0;   // отражение
-        public double Beta { get; set; } = 2.0;    // растяжение
-        public double Gamma { get; set; } = 0.5;   // сжатие
-        public double Sigma { get; set; } = 0.5;   // редукция
-        public double Tolerance { get; set; } = 1e-6; // точность
-        public int MaxIterations { get; set; } = 1000; // максимум итераций
-        public int Dimension { get; set; } // Размерность пространства
-
-        public Parameters() { }
-
-        public Parameters(double alpha, double beta, double gamma,
-                         double sigma, double tolerance, int maxIterations)
-        {
-            Alpha = alpha;
-            Beta = beta;
-            Gamma = gamma;
-            Sigma = sigma;
-            Tolerance = tolerance;
-            MaxIterations = maxIterations;
-        }
+        public double Alpha { get; set; } = 1.0;
+        public double Beta { get; set; } = 2.0;
+        public double Gamma { get; set; } = 0.5;
+        public double Sigma { get; set; } = 0.5;
+        public double Tolerance { get; set; } = 1e-6;
+        public int MaxIterations { get; set; } = 1000;
+        public int Dimension { get; set; } = 0;
     }
 }
