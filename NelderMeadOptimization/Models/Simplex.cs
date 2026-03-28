c35e7545ebdf4c6b96e5a3b69b892f70651abfc8
Merem05 <mashaeremenko2005@gmail.com>
2026-03-28 15:24:29 +0300

    Update Simplex.cs
    Внесла функции (связанные с симплексом)

--- NelderMeadOptimization/Models/Simplex.cs
+++ NelderMeadOptimization/Models/Simplex.cs
@@ -1,29 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
+using System;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace NelderMeadOptimization.Models
 {
     internal class Simplex
     {
-        private readonly Point[] _points;
+        private Point[] _points;
 
-        // Количество точек в симплексе (для N измерений нужно N+1 точек)
         public int Size => _points.Length;
-
-        // Размерность пространства
         public int Dimension => _points[0]?.Dimension ?? 0;
 
-        // Индексатор
+        public Point Best => _points[0];
+        public Point Worst => _points[_points.Length - 1];
+        public Point SecondWorst => _points[_points.Length - 2];
+
         public Point this[int index]
         {
             get => _points[index];
             set => _points[index] = value;
         }
 
-        // Конструктор из массива точек
         public Simplex(Point[] points)
         {
             if (points == null)
@@ -32,38 +28,104 @@ namespace NelderMeadOptimization.Models
             if (points.Length < 2)
                 throw new ArgumentException("Симплекс должен содержать хотя бы 2 точки");
 
-            // Проверяем, что все точки одной размерности
             int dim = points[0].Dimension;
             if (!points.All(p => p.Dimension == dim))
                 throw new ArgumentException("Все точки должны иметь одинаковую размерность");
 
-            // Создаем копии
-            _points = points.Select(p => p.Clone()).ToArray();
+            _points = points.ToArray();
+            Sort();
         }
 
-        // Получение точек по индексам (лучшей и худшей)
-        public Point GetBest() => _points[0];
-        public Point GetWorst() => _points[_points.Length - 1];
-
-        // Получение всех точек кроме последней (все кроме худшей)
-        public Point[] GetAllExceptWorst()
-        {
-            return _points.Take(_points.Length - 1).ToArray();
-        }
-
-        // Сортировка по значению (от лучшей к худшей)
         public void Sort()
         {
             Array.Sort(_points, (a, b) => a.Value.CompareTo(b.Value));
         }
 
-        // Замена худшей точки новой
-        public void ReplaceWorst(Point newPoint)
+        public double[] GetCentroidCoordinates()
         {
-            _points[_points.Length - 1] = newPoint.Clone();
+            int dimension = Dimension;
+            double[] center = new double[dimension];
+
+            for (int i = 0; i < dimension; i++)
+            {
+                center[i] = _points.Take(_points.Length - 1).Average(p => p[i]);
+            }
+
+            return center;
         }
 
-        // Проверка сходимости (все значения близки)
+        public Point Reflect(double[] centroid, Func<double[], double> evaluate, double alpha)
+        {
+            int dimension = Dimension;
+            double[] reflected = new double[dimension];
+
+            for (int i = 0; i < dimension; i++)
+            {
+                reflected[i] = centroid[i] + alpha * (centroid[i] - Worst[i]);
+            }
+
+            return Point.Create(reflected, evaluate);
+        }
+
+        public Point Expand(double[] centroid, Point reflected, Func<double[], double> evaluate, double beta)
+        {
+            int dimension = Dimension;
+            double[] expanded = new double[dimension];
+
+            for (int i = 0; i < dimension; i++)
+            {
+                expanded[i] = centroid[i] + beta * (reflected[i] - centroid[i]);
+            }
+
+            return Point.Create(expanded, evaluate);
+        }
+
+        public Point ContractInside(double[] centroid, Func<double[], double> evaluate, double gamma)
+        {
+            int dimension = Dimension;
+            double[] contracted = new double[dimension];
+
+            for (int i = 0; i < dimension; i++)
+            {
+                contracted[i] = centroid[i] + gamma * (Worst[i] - centroid[i]);
+            }
+
+            return Point.Create(contracted, evaluate);
+        }
+
+        public Point ContractOutside(double[] centroid, Point reflected, Func<double[], double> evaluate, double gamma)
+        {
+            int dimension = Dimension;
+            double[] contracted = new double[dimension];
+
+            for (int i = 0; i < dimension; i++)
+            {
+                contracted[i] = centroid[i] + gamma * (reflected[i] - centroid[i]);
+            }
+
+            return Point.Create(contracted, evaluate);
+        }
+
+        public void Reduce(Func<double[], double> evaluate, double sigma)
+        {
+            for (int i = 1; i < _points.Length; i++)
+            {
+                double[] newCoords = new double[Dimension];
+                for (int j = 0; j < Dimension; j++)
+                {
+                    newCoords[j] = Best[j] + sigma * (_points[i][j] - Best[j]);
+                }
+                _points[i] = Point.Create(newCoords, evaluate);
+            }
+            Sort();
+        }
+
+        public void ReplaceWorst(Point newPoint)
+        {
+            _points[_points.Length - 1] = newPoint;
+            Sort();
+        }
+
         public bool IsConverged(double tolerance)
         {
             double max = _points.Max(p => p.Value);
